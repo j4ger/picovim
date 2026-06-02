579d07b7613fe47fffe3a496a22c96cff2426387
j4ger <xiayuxuan@live.com>
2026-06-02 18:46:39 +0800

    neovide configs

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,3 +1,7 @@
 if vim.g.neovide then
-  vim.o.guifont = 'Maple_Mono_NF_CN_Light:h14, Maple_Mono_NF_CN:h14'
+  vim.o.guifont = 'Maple_Mono_NF_CN_Light:h14'
+  vim.g.neovide_opacity = 0.95
+  vim.g.neovide_normal_opacity = 0.95
+  vim.g.neovide_cursor_animation_length = 0.100
+  vim.g.neovide_cursor_short_animation_length = 0.02
 end
